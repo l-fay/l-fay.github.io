9301832756bf717bbd84876e973283db059bf7f0
lfay <lfyuanwuqu@163.com>
2023-03-13 13:51:41 +0800

    添加笔记本设置

--- scripts/autoSave.js
+++ scripts/autoSave.js
@@ -1,41 +1,45 @@
 require('shelljs/global');
 
 try {
-	hexo.on('deployAfter', function() {//当deploy完成后执行备份
-		run();
-	});
+    hexo.on('deployAfter', function () {//当deploy完成后执行备份
+        run();
+    });
 } catch (e) {
-	console.log("产生了一个错误<(￣3￣)> !，错误详情为：" + e.toString());
+    console.log("产生了一个错误<(￣3￣)> !，错误详情为：" + e.toString());
 }
 
 function run() {
-	if (!which('git')) {
-		echo('Sorry, this script requires git');
-		exit(1);
-	} else {
-		echo("======================Auto Backup Begin===========================");
-		if (process.env.computername === "LQNQ"){
-			cd('F:\\blog');    //此处修改为Hexo根目录路径
-		}else{
-			echo(process.env.computername)
-			echo("请修改openNewMD的判断条件")
-		}
-		
-		if (exec('git add --all').code !== 0) {
-			echo('Error: Git add failed');
-			exit(1);
+    if (!which('git')) {
+        echo('Sorry, this script requires git');
+        exit(1);
+    } else {
+        echo("======================Auto Backup Begin===========================");
+        if (process.env.computername === "LQNQ") {
+            // 台式电脑
+            cd('F:\\blog');    //此处修改为Hexo根目录路径
+        } else if (process.env.computername === "LENOVO") {
+            // 笔记本
+            cd('D:\\blog');
+        } else {
+            echo(process.env.computername)
+            echo("请修改openNewMD的判断条件")
+        }
 
-		}
-		if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
-			echo('Error: Git commit failed');
-			exit(1);
+        if (exec('git add --all').code !== 0) {
+            echo('Error: Git add failed');
+            exit(1);
 
-		}
-		if (exec('git push origin master').code !== 0) {
-			echo('Error: Git push failed');
-			exit(1);
+        }
+        if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
+            echo('Error: Git commit failed');
+            exit(1);
 
-		}
-		echo("==================Auto Backup Complete============================")
-	}
+        }
+        if (exec('git push origin master').code !== 0) {
+            echo('Error: Git push failed');
+            exit(1);
+
+        }
+        echo("==================Auto Backup Complete============================")
+    }
 }
--- scripts/openNewMd.js
+++ scripts/openNewMd.js
@@ -3,8 +3,14 @@ var spawn = require('child_process').exec;
 
 hexo.on('new', function(data){
 if (process.env.computername === "LQNQ"){
-  spawn('start  "D:\Practical_Software\Notepad++\\notepad++.exe" ' + data.path);
-}else{
+  // 台式电脑
+  spawn('start  "D:\\Practical_Software\\Notepad++\\notepad++.exe" ' + data.path);
+}else if (process.env.computername === "LENOVO"){
+  // 笔记本
+  spawn('start  "C:\\Program Files (x86)\\Notepad++\\notepad++.exe" ' + data.path);
+}
+
+else{
   echo(process.env.computername)
   echo("请修改openNewMD的判断条件")
 }
